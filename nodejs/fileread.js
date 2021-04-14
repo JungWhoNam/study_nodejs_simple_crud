0e1540f99b0a37a05254ca36aefff85db7b57d3e
namxx054 <namxx054@umn.edu>
2021-04-14 16:28:06 +0900

    Add a sample code for async file read from fs module

--- nodejs/fileread.js
+++ nodejs/fileread.js
@@ -2,13 +2,21 @@ const fs = require('fs');
 
 const filePath = 'nodejs/sample.txt';
 
-// Getting information for a file
+// Async File Read
 fs.stat(filePath, (error, stats) => {
     if (!error && stats.isFile()) {
         fs.readFile(filePath, 'utf8', (err, data) => {
             if (!err) {
-                console.log(data);
+                console.log('Async:\n', data);
             }
         });
     }
 });
+
+
+// Sync File Read
+let data;
+if (fs.existsSync(filePath)) {
+    data = fs.readFileSync(filePath, {encoding:'utf8', flag:'r'});
+}
+console.log('Sync:\n', data);
